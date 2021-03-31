e6b55d933ea86e441f2ec3647e382b60d1db66b9
zf1976 <1596863112@qq.com>
2021-03-31 15:29:25 +0800

    fix：修复分页删除显示bug

--- src/components/UploaderRecored.vue
+++ src/components/UploaderRecored.vue
@@ -1,47 +1,128 @@
 <template>
-  <el-table
-    :data="tableData"
-    stripe
-    style="width: 100%">
-    <el-table-column
-      prop="date"
-      label="日期"
-      width="180">
-    </el-table-column>
-    <el-table-column
-      prop="name"
-      label="姓名"
-      width="180">
-    </el-table-column>
-    <el-table-column
-      prop="address"
-      label="地址">
-    </el-table-column>
-  </el-table>
+  <el-card>
+    <el-table
+      :data="tableData"
+      stripe
+      highlight-current-row
+      border
+      style="width: 100%"
+      height="500"
+    >
+      <el-table-column
+        fixed
+        prop="name"
+        label="文件名"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="size"
+        label="文件大小"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="md5"
+        label="MD5">
+      </el-table-column>
+      <el-table-column
+        sortable
+        prop="uploadTime"
+        label="上传时间">
+      </el-table-column>
+      <el-table-column label="操作" width="100">
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            type="danger"
+            @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <div class="block">
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :page-count="totalPage"
+        :current-page="pageNumber"
+        :page-size="pageSize"
+        :total="totalRecord"
+        layout="total, prev, pager, next"
+      >
+      </el-pagination>
+    </div>
+  </el-card>
 </template>
 
 <script>
+import axios from "axios";
+import moment from 'moment'
 export default {
   name: "UploaderData",
   data() {
     return {
-      tableData: [{
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-04',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1517 弄'
-      }, {
-        date: '2016-05-01',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1519 弄'
-      }, {
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1516 弄'
-      }]
+      pageSize: 5,
+      pageNumber:1,
+      totalPage: 0,
+      totalRecord:0,
+      tableData: []
+    }
+  },
+  mounted() {
+    this.changePage(this.pageNumber)
+  },
+  methods:{
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`);
+    },
+    changePage(pageNumber){
+      let formData = new FormData();
+      formData.append("pageNumber",pageNumber)
+      formData.append("pageSize", this.pageSize)
+      axios.post('http://localhost:8080/record/page', formData ,{
+        headers:{
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      }).then(res => {
+        let  data = res.data
+        console.log(data)
+        this.pageNumber = data.pageCount
+        this.totalRecord = data.totalRecord
+        this.totalPage = data.totalPage
+        this.tableData = data.data
+      })
+    },
+    handleCurrentChange(val) {
+        this.changePage(val)
+    },
+    handleEdit(index, row) {
+      console.log(index, row);
+    },
+    handleDelete(index, row) {
+      let formData = new FormData()
+      formData.append('id', row.id)
+      axios.post("http://localhost:8080/record/delete", formData , {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      }).then(res => {
+        this.$notify({
+          title: '成功',
+          message: '删除成功',
+          type: 'success'
+        });
+        console.log(index)
+        console.log(this.pageNumber -1 )
+        if (index == 0) {
+          this.pageNumber -1
+          this.changePage(this.pageNumber -1 )
+        }
+        this.changePage(this.pageNumber)
+      }).catch(e => {
+        this.$notify({
+          title: '错误',
+          message: '删除失败',
+          type: 'error'
+        });
+      })
     }
   }
 }
